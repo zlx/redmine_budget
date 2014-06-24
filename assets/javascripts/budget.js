da14f7b101431a27dffbcee5327dbdaa4488b40c
Jacek Tomaszewski <jacek@jtom.me>
2014-06-24 11:48:10 +0200

    Fix js counting of remaining budget and its summary in budget#edit

--- assets/javascripts/budget.js
+++ assets/javascripts/budget.js
@@ -44,7 +44,7 @@ function installBudgetForm($form) {
     $(".js-project_role_budget-hours_count-sum").html(sum);
   }
 
-  function getRoleBudgetHoursCount(roleId) {
+  function getRemainingPlannedHoursCountForRole(roleId) {
     if (!roleId) {
       return 0;
     }
@@ -52,25 +52,58 @@ function installBudgetForm($form) {
     var $select = $(".js-project_role_budget-role_id[value=" + +roleId + "]").last(),
         hoursCount = +$select.closest("tr").find(".js-project_role_budget-hours_count").val() || 0;
 
-    return hoursCount;
+    return hoursCount - (+(workedRoles[roleId] || {}).worked_hours_count || 0);
+  }
+
+  function getCurrentPricePerHourForRole(roleId, wagesType) {
+    var $table = $("table.js-"+ wagesType +"_wages"),
+        wages = $table.find(".js-wage-role_id[value="+ roleId +"]").map(function(){
+          var $row = $(this).closest("tr"),
+              $pricePerHour = $row.find(".js-wage-price_per_hour"),
+              $startDate = $row.find(".js-wage-start_date"),
+              $endDate = $row.find(".js-wage-end_date");
+
+          return {
+            roleId: roleId,
+            pricePerHour: +$pricePerHour.val(),
+            startDate: new Date( $startDate.val() || $startDate.attr("placeholder") ),
+            endDate: new Date( $endDate.val() || $endDate.attr("placeholder") ),
+          };
+        }).toArray();
+
+    wages.sort(function(a, b){
+      return a.startDate - b.startDate;
+    });
+
+    var currentDate = new Date();
+    // 1) get first one of those in present
+    // 2) get first one of those in future
+    // 3) get last one of those in past
+    var wage = wages.filter(function(w){
+      return currentDate >= w.startDate && currentDate <= w.endDate;
+    })[0] || wages.filter(function(w){
+      return currentDate <= w.startDate;
+    })[0] || wages[wages.length - 1];
+
+    return wage ? wage.pricePerHour : 0;
   }
 
   function recountWagesSummary() {
     $(".js-wages").each(function(){
-      $table = $(this);
-
-      var roles = {};
+      var $table = $(this),
+          wagesType = $table.data("wagesType"),
+          roles = {};
 
       var sum = Array.prototype.reduce.call( $table.find(".js-wage-price_per_hour").map(function() {
-        var pricePerHour = +$(this).val(),
-            roleId = $(this).closest("tr").find("select").val(),
-            roleHoursCount = getRoleBudgetHoursCount(roleId);
+        var roleId = +$(this).closest("tr").find("select").val();
 
         if (roles[roleId]) {
           return 0;
         } else {
           roles[roleId] = true;
-          return pricePerHour * roleHoursCount;
+          workedCash = (+(workedRoles[roleId] || {})["worked_" + wagesType] || 0);
+
+          return workedCash + getCurrentPricePerHourForRole(roleId, wagesType) * getRemainingPlannedHoursCountForRole(roleId);
         }
       }), function(x, y){ return x + y; } );
 
