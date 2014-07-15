267d9d23d17de00ff0d9c478ba4a91376aebd845
Jacek Tomaszewski <jacek@jtom.me>
2014-07-15 14:43:24 +0200

    [fix] Fill in time_entry's role by default, if missing

--- lib/espeo_budget/patches/time_entry_patch.rb
+++ lib/espeo_budget/patches/time_entry_patch.rb
@@ -5,6 +5,7 @@ module EspeoBudget::Patches::TimeEntryPatch
 
     base.class_eval do
       belongs_to :role, inverse_of: :time_entries
+      validate :fill_in_role_if_missing
       validates_presence_of :role_id
 
       safe_attributes "role_id"
@@ -15,6 +16,14 @@ module EspeoBudget::Patches::TimeEntryPatch
   end
   
   module InstanceMethods
+    def fill_in_role_if_missing
+      if role_id.nil?
+        member = Member.where(self.attributes.slice("user_id", "project_id")).joins(:roles).first
+        if member && member.roles.present?
+          self.role_id = member.roles.first.id
+        end
+      end
+    end
   end
 end
 
