f4aaaf29e06cda3c87f2e7556d0640d2ec1fd68b
Jacek Tomaszewski <jacek@jtom.me>
2014-08-08 10:46:09 +0200

    Allow migrations to be reverted

--- db/migrate/008_remove_user_id_from_wage_periods.rb
+++ db/migrate/008_remove_user_id_from_wage_periods.rb
@@ -1,6 +1,11 @@
 class RemoveUserIdFromWagePeriods < ActiveRecord::Migration
-  def change
+  def up
     remove_index :wage_periods, :user_id
     remove_column :wage_periods, :user_id
+  end
+
+  def down
+    add_column :wage_periods, :user_id, :integer, null: false
+    add_index :wage_periods, :user_id
   end
 end
