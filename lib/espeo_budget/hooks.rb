c0a36e325bd46c00cb3527f24bc2ec886550b927
Jacek Tomaszewski <jacek@jtom.me>
2014-09-08 14:45:10 +0200

    Fix showing role in timeentry form

--- lib/espeo_budget/hooks.rb
+++ lib/espeo_budget/hooks.rb
@@ -5,7 +5,7 @@ module EspeoBudget
     def view_timelog_edit_form_bottom(context = {})
       time_entry = context[:time_entry]
 
-      context[:controller].send(:render_to_string, {
+      return_value = context[:controller].send(:render_to_string, {
         :partial => "timelog/form_role",
         :locals => context.merge({
           roles_collection: roles_collection_for_select_options(time_entry.user_id, time_entry.project_id)
@@ -23,13 +23,15 @@ module EspeoBudget
           )
         ).uniq
 
-        context[:controller].send(:render_to_string, {
+        return_value << context[:controller].send(:render_to_string, {
           :partial => "timelog/form_user",
           :locals => context.merge({
             users_collection: principals_options_for_select(assignable_users, time_entry.user_id)
           })
         })
       end
+
+      return_value
     end
 
     private
