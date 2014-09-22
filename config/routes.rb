ec386e8aa9bdebf5e5ecf63d1ef927b104b33890
Jacek Tomaszewski <jacek@jtom.me>
2014-09-22 12:24:05 +0200

    Add /budget route as alias to /projects/:project_id/budget

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+get 'budget' => 'budget#show', as: :budget
 get 'projects/:project_id/budget' => 'budget#show', as: :project_budget
 get 'projects/:project_id/budget/settings' => 'budget#edit', as: :edit_project_budget
 post 'projects/:project_id/budget/settings' => 'budget#update'
